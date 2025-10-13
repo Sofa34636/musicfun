f816fbe162488e5448cc9eef7c79e1b49cb31c32
Sofa34636 <www.sofalis21@gmail.com>
2025-10-13 19:50:54 +1000

    при окончании трека включается другой трек

--- src/Track.tsx
+++ src/Track.tsx
@@ -4,12 +4,27 @@ import {NavLink} from "./shared/libs/route/Route.tsx";
 
 type Props = {
     track: SchemaTrackListItemOutput
+    onTrackEnded: (id: string) => void;
+    isSelected: boolean;
+    onTrackPlay: (id: string) => void;
+    setRef: (el: HTMLAudioElement | null) => void;
 }
 
 export function Track(props: Props) {
 
+    // const audioRef = useRef<any>(null);
+
+    const handelTrackEnded = () => {
+        props.onTrackEnded(props.track.id); // сообщает родительскому компоненту (TracksList), что треκ с таким id закончился.
+    }
+    // const handelPlayClick = () => {
+    //     props.onTrackPlay(props.track.id);
+    //     // audioRef.current.play(); // что бы элемент начал играть
+    // }
+
     return (
         <li>
+
             <h4>
                 <NavLink to={'/tracks/' + props.track.id}>
                     {props.track.attributes.title}
@@ -19,6 +34,12 @@ export function Track(props: Props) {
                 // autoPlay={props.isAutoPlay}
                 src={props.track.attributes.attachments[0]!.url}
                 controls={true}
-               />
+                onEnded={handelTrackEnded} // при окончании трека
+                ref={(el) => {
+                    // audioRef.current = el // привязываем к рефу
+                    props.setRef(el) // плеер «передаётся наверх» — родитель сохраняет ссылку на DOM-элемент <audio> для последующего управления (например, чтобы вызвать .play() программно)
+                }}
+            />
+            {/*<button onClick={handelPlayClick}>Play           </button>*/}
         </li>)
 }
--- src/TracksList.tsx
+++ src/TracksList.tsx
@@ -2,11 +2,18 @@ import {useQuery} from "@tanstack/react-query";
 import {client} from "./shared/api/client.ts";
 import {Track} from "./Track.tsx";
 import {useSearchParams} from "react-router";
+import {useRef, useState} from "react"; // API функции для работы с сервером
+
+// const props: Props = {
+//     children: <div></div>
+// }
 
 export function TracksList() {
 
-    const [searchParams] = useSearchParams();
+    let [searchParams] = useSearchParams();
 
+    const [currentTrack, setCurrentTrack] = useState<string | null>(null); // хранит ID сейчас играющего трека.
+    const audioElementRef = useRef<Record<string, HTMLAudioElement | null>>({}); // это объект, где ключ = ID трека, а значение = сам <audio> элемент.
     // делаем запрос к API через useQuery
     // загрузка.FSM типо true\false, но мы предполагаем, что будут дополнительные детали
     const {data, isPending, isError} = useQuery({
@@ -29,13 +36,29 @@ export function TracksList() {
             <hr/>
             Can't load tracks list</div>
     }
-
+    const handelTrackEnded = (id: string) => {
+        const endedIndex = data.data.findIndex((e) => e.id === id);
+        if (endedIndex !== -1) {
+            const nextTrack = data.data[endedIndex + 1];
+            setCurrentTrack(nextTrack!.id); // обновляем состояние
+            audioElementRef.current[nextTrack!.id]!.play() // запускаем следующий трек через ссылку на DOM-элемент
+        }
+    }
+    const handelTrackPlay = (id: string) => {
+        setCurrentTrack(id)
+    }
 
     return <ul>
         {data.data.map((t) => {
             return <Track
                 key={t.id} // уникальный ключ для списка
                 track={t} // сам объект трека
+                onTrackEnded={handelTrackEnded} // после того как трек проигрался до конца вызывается метод
+                isSelected={currentTrack === t.id} // каждому треку передаем true или false, если трек у трек листа совпадет с тем треком, который мы рисуем тогда мы передаем true треку
+                onTrackPlay={handelTrackPlay}
+                setRef={(el)=>{ // сюдо дочерний элемент трек засунет элемент
+                    audioElementRef.current[t.id] = el //он берет пиходящий эл и вкладывает себе в реф
+                }}
             />;
         })
         }
